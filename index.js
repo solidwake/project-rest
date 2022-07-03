316e308b075ad545713f25c8fc23831ced891fa0
Idris Westbrook <westbrook.idris@gmail.com>
2022-07-02 22:35:10 -0400

    add routes for get requests

--- index.js
+++ index.js
@@ -19,13 +19,49 @@ console.log('Listening on port 3000...'); */
 const express = require('express');
 const app = express();
 
+const customers = [
+    {
+        id: 1,
+        name: 'Amuro Ray'
+    },
+    {
+        id: 2,
+        name: 'Char Aznable'
+    },
+    {
+        id: 3,
+        name: 'Kamille Bidan'
+    },
+    {
+        id: 4,
+        name: 'Kou Uraki'
+    },
+
+]
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 
 });
 
 app.get('/api/customers', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(customers);
+});
+
+// Route parameters
+/* app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+}); */
+
+// Query string parameters
+/* app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.query);
+}); */
+
+app.get('/api/customers/:id', (req, res) => {
+    const customer = customers.find(c => c.id === parseInt((req.params.id)));
+    if(!customer) res.status(404).send('The customer was not found');
+    res.send(customer);
 });
 
 const port = process.env.PORT || 3000;
